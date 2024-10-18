cb36a1836c91d7e414eb087cbadffbfbb11dba13
Victor Manuel Ocampo Mendez <l22240018@smartin.tecnm.mx>
2024-10-18 15:04:06 -0600

    Victor Main

---
+++ MainVictor.py
@@ -0,0 +1,12 @@
+
+a = 10
+b = 10
+c = a+b
+Print("Hola Mundo")
+Print(f"\nla suma de {a}+{b} = {c}")
+Print("\nla suma de a+b es",c)
+
+if a>b:
+    print(f"El mayor es {a}")
+else:
+    print(f"El mayor es {b}")
