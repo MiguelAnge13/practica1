e43ece7f727dd6127e580d21d6b9feb477ca0020
Miguel Angel Marquez Mendez <miguel.angel@gmail.com>
2024-10-18 15:16:03 -0600

    variable d y e

--- MainVictor.py
+++ MainVictor.py
@@ -2,6 +2,8 @@
 a = 10
 b = 10
 c = a+b
+d = 10
+e = 10
 Print("Hola Mundo")
 Print(f"\nla suma de {a}+{b} = {c}")
 Print("\nla suma de a+b es",c)
@@ -10,3 +12,4 @@ if a>b:
     print(f"El mayor es {a}")
 else:
     print(f"El mayor es {b}")
+
