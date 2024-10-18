210ce0414f85d32bac24c56f17d2304f3844a8ed
Wendoly <l22240028@smartin.tecnm.mx>
2024-10-18 15:42:40 -0600

    WENDOLY

---
+++ MainWendoly.py
@@ -0,0 +1,12 @@
+
+a = 10
+b = 10
+c = a+b
+print("Hola Mundo")
+print(f"la suma de {a}+{b} = {c}")
+print("\nla suma de a+b es",c)
+
+if a>b:
+    print(f"El mayor es {a}")
+else:
+    print(f"El mayor es {b}")
